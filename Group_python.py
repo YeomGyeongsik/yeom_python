75f0b7270553bb06794d283566ad29bcc2b36739
YeomGyeongsik <80300334+YeomGyeongsik@users.noreply.github.com>
2021-04-23 00:21:53 +0900

    Update Group_python.py

--- Group_python.py
+++ Group_python.py
@@ -1,32 +1,13 @@
-  
+# 5Week Homework - Load Excel File
+
 import openpyxl
-import glob
+import pandas as pd 
 
-xList = glob.glob('./data/*')
-dList = [[] for _ in range(10)]
+xlsxFile = '5week.xlsx' 
+sheetList = [] 
 
-for x in xList:
-	xfd = openpyxl.load_workbook(x, data_only=True)
-	sheet = xfd['Sheet1']
+wb = openpyxl.load_workbook(xlsxFile)
+for i in wb.sheetnames: 
+    sheetList.append(i)
 
-	personDict = {
-		'st_num' : sheet['A2'].value,
-		'name' : sheet['B2'].value,
-		'group_id' : sheet['C2'].value,
-		'git' : sheet['D2'].value
-	}
-	group = int(personDict['group_id'][1:])
-	dList[group-1].append(personDict)
 
-newX = openpyxl.Workbook()
-newX.remove_sheet(newX.get_sheet_by_name('Sheet'))
-
-for i in range(10):
-	newSheet = newX.create_sheet()
-	newSheet.title = 'Sheet%d'%(i+1)
-	newSheet.append(['st_num', 'name', 'group_id', 'git'])
-
-	for s in dList[i]:
-		newSheet.append([str(s['st_num']), s['name'], s['group_id'], s['git']])
-
-newX.save('./group_python.xlsx')
